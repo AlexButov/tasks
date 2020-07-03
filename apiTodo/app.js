e23ef44a5f6c5da8162fb42d73c80125efdadae5
AlexButov <butov@exceedteam@gmail.com>
2020-07-03 15:53:29 +0300

    Все рабочее. Осталось только работа с классом

--- apiTodo/app.js
+++ apiTodo/app.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser') // парсит реквест попадающий в боди
-// const passport = require('passport')
+const passport = require('passport')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
-
+const path = require('path')
 
 const todoRouter = require('./routes/todo')
 const authRouter = require('./routes/auth')
-const exampleRouter = require('./routes/example')
 
 const app = express()
 app.use(cors())
@@ -17,14 +16,18 @@ app.use(bodyParser.json())
 app.use(cookieParser())
 
 
-//app.use(passport.initialize())
-//require('./middleware/passport')(passport)  // Возвращает функцию, которую сразу вызываем, и передаем ей модуль passport
+app.use(passport.initialize())
+require('./middleware/passport')(passport)  // Возвращает функцию, которую сразу вызываем, и передаем ей модуль passport
 
 app.use(express.static(__dirname + '/views/todo'))
-app.use(express.static(__dirname + '/views/example'))
 
 app.use('/api', authRouter)
 app.use('/api/todos', todoRouter)
-app.use('/example', exampleRouter)
+
+
+app.get('/', passport.authenticate('jwt', {session:false}), (req, res) => {
+    res.sendFile('todo.html', {root: path.join(__dirname, '../apiTodo/views/todo')})
+})
+
 
 module.exports = app;
--- apiTodo/routes/todo.js
+++ apiTodo/routes/todo.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/todo')
-// const passport = require('passport')
+const passport = require('passport')
 
 // session:false - значит, что passport.authenticate необходим для каждого запроса
-router.get('/',  controller.getAll)
-router.post('/',  controller.create)
-router.get('/:id',  controller.getById)
-router.put('/:id',  controller.update)
-router.delete('/:id',  controller.remove)
-router.delete('/todos',  controller.clearDb)
+router.get('/', passport.authenticate('jwt', {session:false}),  controller.getAll)
+router.post('/', passport.authenticate('jwt', {session:false}), controller.create)
+router.get('/:id', passport.authenticate('jwt', {session:false}), controller.getById)
+router.put('/:id', passport.authenticate('jwt', {session:false}), controller.update)
+router.delete('/:id', passport.authenticate('jwt', {session:false}), controller.remove)
+router.delete('/todos', passport.authenticate('jwt', {session:false}), controller.clearDb)
 
 module.exports = router
 
-// passport.authenticate('jwt', {session:false}),
